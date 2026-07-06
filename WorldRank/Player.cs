055ce31a0e00f421aa34990e313be68bdc286b72
ava1313 <stratosav1999@gmail.com>
2026-07-06 23:20:03 +0300

    Add player registry console app

---
+++ WorldRank/Player.cs
@@ -0,0 +1,14 @@
+﻿public class Player
+{
+    public int Id { get; private set; }
+    public string Name { get; set; }
+
+    public int Score { get; set; }
+
+    public Player(int id, string name, int score)
+    {
+        Id=id;
+        Name = name;
+        Score = score;
+    }
+}
--- WorldRank/Program.cs
+++ WorldRank/Program.cs
@@ -1,1 +1,80 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+List<Player> players = new List<Player>();
+
+
+int nextId = 1;
+bool running = true;
+
+
+
+    while (running)
+    {
+        Console.WriteLine("1. Add Player");
+        Console.WriteLine("2. Display Players");
+        Console.WriteLine("3. Find player by name");
+        Console.WriteLine("4. Exit");
+        Console.Write("Select an option: ");
+        
+
+
+
+    string inputText = Console.ReadLine();
+    Console.WriteLine("");
+    
+    int input = int.Parse(inputText);
+   
+        if (input == 1)
+        {
+            Console.Write("Player name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Player score: ");
+            string scoreText = Console.ReadLine();
+            int score = int.Parse(scoreText);
+
+            Player player = new Player(nextId, name, score);
+            players.Add(player);
+
+            nextId++;
+        Console.WriteLine("===========================================");
+        Console.WriteLine("        Player added successfully.");
+        Console.WriteLine("===========================================");
+
+        }
+        else if (input == 2)
+        {
+            Console.WriteLine("Players:");
+            foreach (var player in players)
+            {
+                Console.WriteLine("Id: " + player.Id + ", Name: " + player.Name + ", Score: " + player.Score);
+            }
+        }
+            else if (input == 3)
+        {
+            Console.Write("Enter player name to search: ");
+            string searchName = Console.ReadLine();
+            var foundPlayer = players.FirstOrDefault(p => p.Name.Equals(searchName, StringComparison.OrdinalIgnoreCase));
+            if (foundPlayer != null)
+            {
+            Console.WriteLine("===========================================");
+            Console.WriteLine("Found Player - Id: " + foundPlayer.Id + ", Name: " + foundPlayer.Name + ", Score: " + foundPlayer.Score);
+            Console.WriteLine("===========================================");
+
+        }
+            else
+            {
+             Console.WriteLine("===========================================");
+             Console.WriteLine("             Player not found.");
+             Console.WriteLine("===========================================");
+
+        }
+    }
+        else if (input == 4)
+        {
+            running = false;
+        }
+        else
+        {
+            Console.WriteLine("Invalid option. Please try again.");
+        }
+    }
