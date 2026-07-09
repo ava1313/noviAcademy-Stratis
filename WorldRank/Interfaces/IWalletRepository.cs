707669482396e6493b3925c7df0e744e9bb4c1bb
ava1313 <stratosav1999@gmail.com>
2026-07-09 10:39:16 +0300

    Errors

--- WorldRank/Interfaces/IWalletRepository.cs
+++
@@ -1,9 +0,0 @@
-using WorldRank.Classes;
-
-namespace WorldRank.Interfaces;
-
-public interface IWalletRepository
-{
-    void Add(Wallet wallet, int playerId);
-    List<Wallet> GetByPlayer(int playerId);
-}
