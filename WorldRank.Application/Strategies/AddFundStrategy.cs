ddb713169a066961de46fde5fa2e785ce9203a10
ava1313 <stratosav1999@gmail.com>
2026-07-09 17:02:27 +0300

    Strategies

---
+++ WorldRank.Application/Strategies/AddFundStrategy.cs
@@ -0,0 +1,13 @@
+﻿using WorldRank.Domain;
+
+namespace WorldRank.Application.Strategies;
+
+internal class AddFundStrategy : IFundsStrategy
+{
+    public FundsOperation Operation => FundsOperation.Add;
+
+    public void Execute(Wallet wallet, decimal amount)
+    {
+        wallet.Deposit(amount);
+    }
+}
---
+++ WorldRank.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldRank.Infrastructure
+{
+    internal class DependencyInjection
+    {
+    }
+}
